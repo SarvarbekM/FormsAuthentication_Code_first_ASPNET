4847b2b51e968f1156379bd2a091540bc5e8a1b0
SarvarbekM <sarvarbek.matvapayev@gmail.com>
2020-01-10 00:14:45 +0500

    first commit

---
+++ FormsAuthApp/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using FormsAuthApp.Models;
+
+namespace FormsAuthApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.User.Where(u => u.Email == model.Name && u.Password == model.Password).FirstOrDefault();
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Register", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.User.Where(u => u.Email == model.Name && u.Password == model.Password).FirstOrDefault();
+                }
+                if (user == null)
+                {
+                    // создаем нового пользователя
+                    using (UserContext db = new UserContext())
+                    {
+                        db.User.Add(new User { Email = model.Name, Password = model.Password, Age = model.Age });
+                        db.SaveChanges();
+
+                        user = db.User.Where(u => u.Email == model.Name && u.Password == model.Password).FirstOrDefault();
+                    }
+                    // если пользователь удачно добавлен в бд
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Name, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+        //
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ FormsAuthApp/Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.Entity;
+
+namespace FormsAuthApp.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() :
+            base(@"Data Source=SARVARBEK\SQLEXPRESS;Initial Catalog=Authentication;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework")
+        { }
+
+        public DbSet<User> User { get; set; }
+    }
+}
